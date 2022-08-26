b1bc5a867f20285e29f63028100a78f93dab523e
unknown <gritsunov85@gmail.com>
2022-08-26 17:49:13 +0600

    end

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿int[] TotalArray(int col)
+{
+    int[] array = new int[col];
+    int i = 0;
+    while (i < array.Length)
+    {
+        Random rand = new Random();
+        array[i] = rand.Next(1, 10);
+        Console.WriteLine(Convert.ToString(array[i]) + ", ");
+        i++;
+    }
+    return array;
+}
+
+
+
+Console.WriteLine("Введите длинну массива: ");
+int total = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("");
+int[] mass = TotalArray(total);
+int m = 0;
+for(int i = 0; i < mass.Length; i++){
+    if(i % 2 == 1){
+        m = m + mass[i];
+    } 
+}
+
+Console.Write("Ответ: " + Convert.ToInt32(m));
